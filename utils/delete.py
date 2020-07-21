75a4bbfeab959d68fbba5e59acd4e98ed5759226
Asa Schachar <asa@optimizely.com>
2020-07-21 14:38:32 -0700

    Fix delete script

--- utils/delete.py
+++ utils/delete.py
@@ -1,23 +1,36 @@
-import labs_constants
 import os
 
-SLUG_TO_DELETE = os.environ['SLUG_TO_DELETE']
+import labs_constants
 
-def main():
+import contentful_management
+
+def delete_lab(slug):
+  '''
+  Deletes a lab specified by the slug parameter
+
+  Args:
+    slug: string specifying the slug of the lab
+  '''
   print('DELETING A SPECIFIC LAB')
-
-  print('======= Step 0 =======')
-  print('Find the lab ')
   client = contentful_management.Client(labs_constants.CONTENTFUL_MANAGEMENT_TOKEN)
-  entry = client.entries(labs_constants.CONTENTFUL_SPACE_ID, labs_constants.CONTENTFUL_ENVIRONMENT).find(slug)
+  entry = None
+  try:
+    print('Finding the lab %s...' % slug)
+    entry = client.entries(labs_constants.CONTENTFUL_SPACE_ID, labs_constants.CONTENTFUL_ENVIRONMENT).find(slug)
   except contentful_management.errors.NotFoundError:
-    print('Entry not found for slug %s, done with delete step')
+    print('Entry not found for slug %s, STOPPING delete step')
     return
   else:
     print('Found existing entry for slug %s, deleting...' % slug)
     entry.unpublish()
     entry.delete()
 
-  print('Finished DELETE for slug %s' % slug)
+  print('FINISHED delete for slug %s' % slug)
+
+
+def main():
+  SLUG_TO_DELETE = os.environ['SLUG_TO_DELETE']
+  delete_lab(SLUG_TO_DELETE)
+
 
 main()
